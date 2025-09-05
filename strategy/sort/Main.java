c7f4a572daf39499396e9a8012f843e85fc8389e
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-09-05 19:22:43 +0300

    Tests are done

--- strategy/sort/Main.java
+++ strategy/sort/Main.java
@@ -1,5 +1,7 @@
 package sort;
 
+import sort.sorting_algorithms.BubbleSort;
+import sort.sorting_algorithms.InsertionSort;
 import sort.sorting_algorithms.SelectionSort;
 
 import java.util.Arrays;
@@ -8,11 +10,48 @@ public class Main {
     public static void main(String[] args) {
         SortContext sortContext = new SortContext(new SelectionSort());
 
-        int[] test = createRandomArray(30, 100);
-        System.out.println(Arrays.toString(test));
+        System.out.println("Creating dataset");
 
-        int[] sortedTest = sortContext.sortArray(test);
-        System.out.println(Arrays.toString(sortedTest));
+        int[] set1 = createRandomArray(90, 300);
+        int[] set2 = createRandomArray(100000, 50000);
+
+        System.out.println("Testing small array:");
+        System.out.println(Arrays.toString(set1));
+
+
+        System.out.println();
+        System.out.println("Testing selection sort...");
+        testSortingAlgorithm(sortContext, set1);
+
+        System.out.println();
+        System.out.println("Testing insertion sort...");
+        sortContext.setStrategy(new InsertionSort());
+        testSortingAlgorithm(sortContext, set1);
+
+        System.out.println();
+        System.out.println("Testing bubble sort...");
+        sortContext.setStrategy(new BubbleSort());
+        testSortingAlgorithm(sortContext, set1);
+
+        System.out.println();
+        System.out.println("=".repeat(50));
+        System.out.println();
+        System.out.println("Testing large array:");
+
+        System.out.println();
+        System.out.println("Testing selection sort...");
+        sortContext.setStrategy(new SelectionSort());
+        testSortingAlgorithm(sortContext, set2);
+
+        System.out.println();
+        System.out.println("Testing insertion sort...");
+        sortContext.setStrategy(new InsertionSort());
+        testSortingAlgorithm(sortContext, set2);
+
+        System.out.println();
+        System.out.println("Testing bubble sort...");
+        sortContext.setStrategy(new BubbleSort());
+        testSortingAlgorithm(sortContext, set2);
     }
 
     public static int[] createRandomArray(int n, int max) {
@@ -23,8 +62,14 @@ public class Main {
         return arr;
     }
 
-    public static void testSortingAlgorithm() {
-        int[] set1 = createRandomArray(30, 100);
-        int[] set2 = createRandomArray(100000, 5000);
+    public static void testSortingAlgorithm(SortContext sortContext, int[] set) {
+        int[] setCopy = Arrays.copyOf(set, set.length);
+        double startTime = System.nanoTime();
+        int[] _z = sortContext.sortArray(setCopy);
+        double endTime = System.nanoTime();
+        double totalTime = endTime - startTime;
+        // Asked ChatGPT to remove scientific notation for better readability (for tiny numbers)
+        System.out.printf("Total time taken: %.9f s%n", totalTime / 1_000_000_000.0);
+
     }
 }
