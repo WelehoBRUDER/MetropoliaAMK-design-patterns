2a37f94b79f139378e4aa99df255d4715f9b0cd2
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-10-07 08:05:08 +0300

    Initialize fibonacci task

---
+++ iterator/FibonacciIterator.java
@@ -0,0 +1,13 @@
+import java.util.Iterator;
+
+public class FibonacciIterator implements Iterator<Integer> {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Integer next() {
+        return 0;
+    }
+}
