fed581b61c48fd77821cffe6a0f5f658b875d9b8
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-10-07 14:59:40 +0300

    idk not fixed

--- pixel-art/src/main/java/PixelCanvas.java
+++ pixel-art/src/main/java/PixelCanvas.java
@@ -6,7 +6,7 @@ public class PixelCanvas {
     private Canvas canvas;
     private GraphicsContext gc;
     private final int PIXEL_SIZE = 32;
-    private final int LINE_WIDTH = 2;
+    private final int LINE_WIDTH = 4;
 
     public PixelCanvas(int width, int height) {
         int xSize = width * PIXEL_SIZE;
@@ -24,18 +24,20 @@ public class PixelCanvas {
     }
 
     public void drawPixel(int x, int y, boolean filled, boolean selected) {
-        x *= PIXEL_SIZE;
-        y *= PIXEL_SIZE;
-        System.out.println("x: " + x + ", y: " + y + ", filled: " + filled + ", selected: " + selected);
-        this.gc.clearRect(x, y, PIXEL_SIZE, PIXEL_SIZE);
+        x = (int) Math.round((double) x * PIXEL_SIZE);
+        y = (int) Math.round((double) y * PIXEL_SIZE);
+        int HALF_LINE = (int) Math.round((double) LINE_WIDTH / 2);
+        double epsilon = 0.1;
+        double epsilon2 = epsilon * 2;
+        this.gc.clearRect(x + epsilon, y + epsilon, PIXEL_SIZE + epsilon2, PIXEL_SIZE + epsilon2);
         if (filled) {
             this.gc.setFill(Color.BLACK);
-            this.gc.fillRect(x, y, PIXEL_SIZE, PIXEL_SIZE);
+            this.gc.fillRect(x + epsilon, y + epsilon, PIXEL_SIZE + epsilon2, PIXEL_SIZE + epsilon2);
         }
         if (selected) {
             this.gc.setLineWidth(LINE_WIDTH);
             this.gc.setStroke(Color.SILVER);
-            this.gc.strokeRect(x + LINE_WIDTH / 2, y + LINE_WIDTH / 2, PIXEL_SIZE - LINE_WIDTH, PIXEL_SIZE - LINE_WIDTH);
+            this.gc.strokeRect(x + HALF_LINE, y + HALF_LINE, PIXEL_SIZE - LINE_WIDTH, PIXEL_SIZE - LINE_WIDTH);
         }
     }
 }
