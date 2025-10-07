57119c0c59b6ab116c5f304257e432ca59b28860
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-10-07 13:34:16 +0300

    Command initialized

---
+++ pixel-art/src/main/java/DrawingBoard.java
@@ -0,0 +1,51 @@
+public class DrawingBoard {
+    private int[][] board;
+    private int x;
+    private int y;
+
+    public DrawingBoard(int width, int height) {
+        board = new int[height][width];
+        x = 0;
+        y = 0;
+    }
+
+    public void toggle() {
+        board[y][x] = board[y][x] == 1 ? 0 : 1;
+    }
+
+    public void moveCursorUp() {
+        y -= 1;
+        if (y < 0) y = 0;
+    }
+
+    public void moveCursorDown() {
+        y += 1;
+        if (y >= board.length) y = board.length - 1;
+    }
+
+    public void moveCursorLeft() {
+        x -= 1;
+        if (x < 0) x = 0;
+    }
+
+    public void moveCursorRight() {
+        x += 1;
+        if (x >= board.length) x = board.length - 1;
+    }
+
+    public void generateCode() {
+        System.out.println("int[][] pixelArt = {");
+        for (int y = 0; y < board.length; y++) {
+            System.out.print("\t{");
+            for (int x = 0; x < board.length; x++) {
+                if (x == board[y].length - 1) {
+                    System.out.print(board[y][x]);
+                } else {
+                    System.out.print(board[y][x] + ", ");
+                }
+            }
+            System.out.print("},\n");
+        }
+        System.out.println("};");
+    }
+}
---
+++ pixel-art/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        DrawingBoard db = new DrawingBoard(8, 8);
+        db.generateCode();
+    }
+}
